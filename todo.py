26c6420d1280290bde0a37600f6ed25799ec6171
Jatin Popli <jatinpopli196@gmail.com>
2020-12-17 13:14:31 +0530

    13 test cases passed

--- todo.py
+++ todo.py
@@ -6,59 +6,101 @@ def TodoAdd(task):
     with open("todo.txt", 'a+') as todoFile:
         try:
             todoFile.write(task+'\r')
-            print("Added Task : ", task)
+            print('Added todo: "{0}"'.format(task), end=' ')
         except:
             print("An Error Occured")
         
 def ReadTodo():
-    with open("todo.txt", 'r') as f:
-        lines = f.readlines()
-        Length = len(lines)
-        for idx, line in enumerate(reversed(lines)):
-            print('[{0}] : {1}'.format(Length-idx, line), end='')
 
+    try:
+        with open("todo.txt", 'r') as f:
+            lines = f.readlines()
+            Length = len(lines)
+            # if Length==0:
+                
+            for idx, line in enumerate(reversed(lines)):
+                print('[{0}] {1}'.format(Length-idx, line), end='')
+    except:
+        print("There are no pending todos!")
+        exit()
+        
 def ReadHelp():
     with open("help.txt", 'r') as h:
         for line in h:
             print(line, end='')
 
+
 def DelTask(num):
-        num = int(num)
-        with open("todo.txt", 'r') as f:
-            lines = f.readlines()
-            print("lines before deleting ", lines)
-            Length = len(lines)
+    num = int(num)
+    lines=[]
+    with open("todo.txt", 'r') as f:
+        lines = f.readlines()
+        Length = len(lines)
+
+        try:
+            TaskToDel = lines[num-1]
+            lines.remove(TaskToDel)
+            print('Deleted todo #{0}'.format(num))
+        except:
+            print("Error: todo #0 does not exist. Nothing deleted.".format(num))
+            exit()
+
+    with open("todo.txt", 'a') as f:
+        f.truncate(0)
+        for task in lines:
             try:
-                TaskToDel = lines[num-1]
-                lines.remove(TaskToDel)
-                print("lines after deleting ", lines)
-
-                print('[{0}] {1} deleted'.format(num, TaskToDel))
-                with open("todo.txt", 'a+') as todoFile:
-                    todoFile.truncate(0)
-                    for task in lines:
-                        try:
-                            todoFile.write(task)
-                        except:
-                            print("An Error Occured")
-                return TaskToDel
+                f.write(task)
             except:
-                print("Task Don't exist")
+                print("An Error Occured")
+        
+        
 
+def MarkTodo(num):
+    num = int(num)
+    TaskDone = ""
+    with open("todo.txt", 'r') as f:
+        lines = f.readlines()
+        Length = len(lines)
+
+        try:
+            TaskDone = lines[num-1]
+            lines.remove(TaskDone)
+            print('Marked todo #{0} as done.'.format(num))
+        except:
+            print("Error: todo #{0} does not exist.".format(num))
+            exit()
+    with open("todo.txt", 'a') as f:
+        f.truncate(0)
+        for task in lines:
+            try:
+                f.write(task)
+            except:
+                print("An Error Occured")       
+                exit()
+    with open("done.txt", 'a+') as DoneFIle:
+        try:
+            DoneFIle.write(TaskDone+'\r')
+        except:
+            print("An Error Occured")
 
 
 
 if len(sys.argv) > 3:
     print("You have specified too many arguments")
     exit()
-arg = sys.argv[1]
 
-if(arg=="add"):
+arg = ""
+try:
+    arg=sys.argv[1]
+except:
+    ReadHelp()
+
+if (arg=="add"):
     task = ""
     try:
         task = sys.argv[2]
     except:
-        print("You didn't specified task")
+        print("Error: Missing todo string. Nothing added!")
         exit()
     TodoAdd(task)
 elif (arg =="ls"):
@@ -69,10 +111,15 @@ elif (arg=="del"):
     try:
         taskNum = sys.argv[2]
     except:
-        print("You didn't specified task number to delete")
+        print("Error: Missing NUMBER for deleting todo.")
         exit()
     DelTask(taskNum)
 # elif (arg == "report"):
 #     Report()
-# elif (arg=="done"):
-#     MarkTodo()
+elif (arg=="done"):
+    try:
+        taskNum = sys.argv[2]
+    except:
+        print("Error: Missing NUMBER for marking todo as done.")
+        exit()
+    MarkTodo(taskNum)
